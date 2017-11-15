5c9f6ca41e38c3000282a22c53c1ff5a7fe44772
2151475 <2151475@my.ipleiria.pt>
2017-11-15 18:42:24 +0000

    main pronto

--- app/src/main/java/com/taes/iair/iair/LocaisActivity.java
+++ app/src/main/java/com/taes/iair/iair/LocaisActivity.java
@@ -2,6 +2,9 @@ package com.taes.iair.iair;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class LocaisActivity extends AppCompatActivity {
 
@@ -9,5 +12,9 @@ public class LocaisActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_locais);
+
+
+
+
     }
 }
