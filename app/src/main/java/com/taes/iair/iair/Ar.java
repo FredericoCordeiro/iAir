97941c584f45d6f6301611f24107bbbaccf38a92
Pedro <pedrosilva97@gmail.com>
2017-11-17 13:48:34 +0000

    inicio de requests à api ThingVIew

---
+++ app/src/main/java/com/taes/iair/iair/Ar.java
@@ -0,0 +1,18 @@
+package com.taes.iair.iair;
+
+/**
+ * Created by Pedro on 17/11/2017.
+ */
+
+public class Ar {
+    private int humidade;
+    private int temperatura;
+    private int pressAtmosferica;
+
+    public Ar(int humidade, int temperatura, int pressAtmosferica) {
+        this.humidade = humidade;
+        this.temperatura = temperatura;
+        this.pressAtmosferica = pressAtmosferica;
+    }
+
+}
--- app/src/main/java/com/taes/iair/iair/MainActivity.java
+++ app/src/main/java/com/taes/iair/iair/MainActivity.java
@@ -4,6 +4,17 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +23,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getData();
+    }
 
+    public Ar getData(){
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="https://api.thingspeak.com/channels/365262/feeds.json?results=2";
+
+// Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        //mTextView.setText("Response is: "+ response.substring(0,500));
+                        //Toast.makeText(MainActivity.this, response, Toast.LENGTH_LONG).show();
+                        try {
+                            JSONObject mainObject = new JSONObject(response);
+                            JSONObject feeds = mainObject.getJSONObject("feeds");
+                            String  cidade = feeds.getString("field1");
+                            Toast.makeText(MainActivity.this, cidade.toString(), Toast.LENGTH_SHORT).show();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
+
+        return null;
     }
 
     public void onClickLocais(View view) {
